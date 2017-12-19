63ed9bb247e218d8086f2abb9b8406ad5a3d40b2
Glen Kallhoff <glen@gallup.com>
2017-12-19 22:29:36 +0000

    the second version of our application

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@
 import os
 
 # Title of the site
-SITE_TITLE = 'Glens Docker Lab'
+SITE_TITLE = 'Glens Docker Lab next version'
 # The URL for static files/images
 STATIC_URL = 'https://s3-us-west-2.amazonaws.com/flaskdockerlab-static/'
 # The name of the
